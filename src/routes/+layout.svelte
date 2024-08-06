60344f3e0b616865e8be279b44573109aa821729
Javier Bullrich <javier@bullrich.dev>
2024-08-06 11:59:56 +0100

    added footer (#7)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,12 @@
 </script>
 
 <slot />
+<footer class="footer footer-center bg-base-300 text-base-content p-4 absolute bottom-0">
+	<aside>
+		<p>
+			Made with ❤️ by <a class="text-primary link hover:text-secondary" href="https://bullrich.dev"
+				>Bullrich</a
+			>
+		</p>
+	</aside>
+</footer>
